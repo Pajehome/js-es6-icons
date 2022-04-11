eb86de1b48322e21e815b9debd49b6db3b48f5ba
Pajehome <abdel20069@gmail.com>
2022-04-11 16:48:58 +0200

    opzioni

--- data.js
+++ data.js
@@ -115,13 +115,6 @@ const tipo = [
 ];
 
 
-// Milestone 1
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
-// Milestone 2
-// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-// Milestone 3
-// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-
  let mainContainer = document.querySelector('.main-container')
 
  let mainCard = '';
@@ -142,7 +135,14 @@ const tipo = [
 console.log(mainCard);
 
 
-const x =document.getElementById('opzioni').value
-if(x === '1'){
-	
+const opzioni = document.getElementById('opzioni').value
+
+if(opzioni === '1'){
+	let tipoAnimale = tipo.filter((genere) => genere.type === 'animal')
+}
+else if(opzioni === '2'){
+	let tipoVegetale = tipo.filter((genere) => genere.type === 'vegetable')
+}
+else if(opzioni === '3'){
+	let tipoUser = tipo.filter((genere) => genere.type === 'user')
 }
